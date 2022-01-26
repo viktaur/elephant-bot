29f9b462e40339b8c5dc73d28fec1997bfb62d22
viktaur <victorgabal@gmail.com>
2022-01-26 19:44:37 +0000

    it works!

--- commands/zepler.js
+++ commands/zepler.js
@@ -2,29 +2,29 @@
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-var exec = require('child_process').exec;
-function execute(command, callback){
-    exec(command, function(error, stdout, stderr){ callback(stdout); });
-};
+const execute = require('child_process').exec;
 
-const { promisify } = require('util');
-const exec = promisify(require('child_process').exec);
+let rawOutput;
+
+cback = (err, stdout, stderror) => {
+    rawOutput = stdout;
+}
+
+execute('arp -a', cback);
 
 function getZeplerStatus () {
-    var rawOutput = await execute('arp -a');
-    // rawOutput: rawOutput.stdout.trim() // I'll leave it commented since I don't know what this does
 
     var outputArray = rawOutput.split("\n");
     
     if (outputArray.length > 50) {
-       return ("The number of computers that are on right now is", outputArray.length, "\nZEPLER IS CROWDED");
+       return ("The number of computers that are on right now is " + outputArray.length + "\nZEPLER IS CROWDED");
     } else {
-       return ("The number of computers that are on right now is", outputArray.length, "\nZEPLER IS NOT CROWDED");
+       return ("The number of computers that are on right now is " + outputArray.length + "\nZEPLER IS NOT CROWDED");
     }
 };
 
 module.exports = {
-    data: new SlashCommandBuilder
+    data: new SlashCommandBuilder()
         .setName("zepler")
         .setDescription("Returns the number of computers logged in at Zepler and whether it's crowded or not"),
     async execute(interaction) {
