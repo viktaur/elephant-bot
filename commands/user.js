99f18bc7ad523e2365b6771ef1e8f1b3b57818ff
viktaur <victorgabal@gmail.com>
2022-01-26 19:49:30 +0000

    asld

--- commands/user.js
+++ commands/user.js
@@ -1,17 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-var socialCreditScore = new Map();
+// var socialCreditScore = new Map();
 
-guild.members.forEach(member => () => {
-   var indSCS = socialCreditScore.set(member.user.username, 800);
-});
+// guild.members.forEach(member => () => {
+//    var indSCS = socialCreditScore.set(member.user.username, 800);
+// });
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
 		.setDescription('Replies with user info!'),
 	async execute(interaction) {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}\nYour dignity: ${dignity}\nYour social credit score: ${socialCreditScore}\nSince you ask so much your social credit will be reduced *(-20 social credit score)*.`);
-        socialCreditScore.set(member.user.username, indSCS - 20);
+		// await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}\nYour dignity: ${dignity}\nYour social credit score: ${socialCreditScore}\nSince you ask so much your social credit will be reduced *(-20 social credit score)*.`);
+        // socialCreditScore.set(member.user.username, indSCS - 20);
+		await interaction.reply(`hello ${interaction.user.tag}`)
 	},
 };
