b5139903ffd8956491ea2d7351bc4bc588a3aaea
viktaur <victorgabal@gmail.com>
2022-02-09 00:11:25 +0000

    solve #7

--- commands/sum.js
+++ commands/sum.js
@@ -1,16 +1,22 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
-// Change these two later
-var a = 1; 
-var b = 2;
-
-var sum = a + b;
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('sum')
-		.setDescription('Adds two integers.'),
+		.setDescription('Adds two integers.')
+		.addStringOption(option =>
+			option.setName('input')
+			.setDescription('Usage: name land')
+			.setRequired(true)),
 	async execute(interaction) {
+		let option = interaction.options.get("input");
+		var args = option.value.split(" ");
+
+		var a = parseInt(args[0]);
+		var b = parseInt(args[1]);
+
+		var sum = a + b;
+
 		await interaction.reply(`${sum}`);
 	},
 };
